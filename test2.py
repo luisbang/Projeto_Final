3c5aac846389d1b11594773e96116b8381938aeb
luisbang91 <yeongbae91@gmail.com>
2021-05-20 11:21:56 -0300

    asd

--- test2.py
+++ test2.py
@@ -1,21 +1,22 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import yfinance
-import datetime
-from fbprophet import Prophet
-from fbprophet.plot import plot_plotly, plot_components_plotly
-from dotenv import load_dotenv
+#import pandas as pd
+#import numpy as np
+#import seaborn as sns
+#import yfinance
+#from plotly.subplots import make_subplots
+#import datetime
+#from fbprophet import Prophet
+#from fbprophet.plot import plot_plotly, plot_components_plotly
+#from dotenv import load_dotenv
 
-import os
-import tweepy as tw
-from textblob import TextBlob
-from wordcloud import WordCloud
-import re
-from googletrans import Translator
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+#import os
+#import tweepy as tw
+#from textblob import TextBlob
+#from wordcloud import WordCloud
+#import re
+#from googletrans import Translator
+#import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+
 
 st.write('oi')
